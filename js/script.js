93e935a1e39ffbb12bf2504097ae127f0af6fa11
hendi <hendihandoko47@gmail.com>
2023-08-21 00:44:53 +0700

    Add pop up modal for lineup content

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav"); //untuk mencari elemen yang nama classnya navbar-nav
 const links = navbarNav.querySelectorAll("a");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const openModalDivs = document.querySelectorAll(".open-modal-link"); // Update this selector
+  const closeModalButtons = document.querySelectorAll(".close-modal-btn");
+  const modals = document.querySelectorAll(".modal");
+
+  openModalDivs.forEach((div) => {
+    // Change variable name to openModalDivs
+    div.addEventListener("click", (event) => {
+      event.preventDefault(); // Prevent the default link behavior
+      const modalId = div.getAttribute("data-modal");
+      const modal = document.getElementById(modalId);
+      modal.style.display = "block";
+    });
+  });
+
+  closeModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = button.closest(".modal");
+      modal.style.display = "none";
+    });
+  });
+
+  window.addEventListener("click", (event) => {
+    modals.forEach((modal) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+});
